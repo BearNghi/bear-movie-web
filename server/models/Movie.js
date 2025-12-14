8cb817bae47704e393249eeec91836226e038f8b
bearnghi <nghibear3010@gmail.com>
2025-12-14 10:25:30 +0700

    Final: Hoan thien chuc nang Admin sua phim, sua loi chuyen huong login va sua loi casing Comment.js

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -6,18 +6,18 @@ const MovieSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    desc: { type: String }, // Mô tả phim
-    img: { type: String }, // Ảnh bìa phim
-    imgTitle: { type: String }, // Ảnh tên phim (logo)
-    imgSm: { type: String }, // Ảnh nhỏ (thumbnail)
-    trailer: { type: String }, // Link trailer
-    video: { type: String }, // Link phim chính (QUAN TRỌNG)
-    year: { type: String }, // Năm sản xuất
-    limit: { type: Number }, // Độ tuổi giới hạn (ví dụ: 16+)
-    genre: { type: String }, // Thể loại (Hành động, Hài...)
+    desc: { type: String },
+    img: { type: String },
+    imgTitle: { type: String },
+    imgSm: { type: String },
+    trailer: { type: String },
+    video: { type: String },
+    year: { type: String },
+    limit: { type: Number },
+    genre: { type: String },
     isSeries: {
         type: Boolean,
-        default: false // Mặc định là phim lẻ
+        default: false
     }
 }, { timestamps: true });
 
